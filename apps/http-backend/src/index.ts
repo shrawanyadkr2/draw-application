04a32d926c64c574329656b75933dff1a652d8a8
shrawan <shrawan29yadav@gmail.com>
2026-02-21 13:15:35 +0530

    step 12

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,3 +1,36 @@
 import express from "express";
 const app= express();
-app.listen(3000);
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
+
+app.post("/signup", (req,res)=>{
+
+    
+
+})
+
+app.post("/signin",(req,res)=>{
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.json({
+        token
+    })
+
+})
+
+app.post("/room",middleware,(req,res)=>{
+    //db calls
+    
+
+
+    res.json({
+        roomId : 123
+    })
+
+})
+app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,31 @@
-import {WebSocketServer} from "ws";
-const wss = new WebSocketServer({port:8080});
+import { WebSocketServer } from "ws";
+import { JWT_SECRET } from "./config";
+import  jwt, { JwtPayload }  from "jsonwebtoken";
 
-wss.on('connection',function connection(ws){
-    ws.on('message',function message(data){
-        ws.send('pong');
-    })
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+
+  if (!url) {
+    return;
+  }
+
+  const queryString = url.split("?")[1];
+
+  const queryParams = new URLSearchParams(queryString);
+  const token = queryParams.get("token") || "";
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if(!decoded || !(decoded as JwtPayload).userId){
+    ws.close();
+    return;
+  }
+
+  console.log("Token:", token);
+
+  ws.on("message", function message(data) {
+    ws.send("pong");
+  });
 });
