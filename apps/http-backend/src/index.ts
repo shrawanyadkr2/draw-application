b8027282b4b15b0a93fcb66455f80fef58a8b55b
shrawan <shrawan29yadav@gmail.com>
2026-02-21 15:26:27 +0530

    debug

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,39 @@
 import express from "express";
 const app= express();
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types";
+
 
 app.post("/signup", (req,res)=>{
 
+    const data= CreateUserSchema.safeParse(req.body);
+
+    if(!data.success){
+        res.json({
+            message:"Incorrect Input"
+        })
+        return
+    }
     
+    //db call 
+    res.json({
+        userId: "123"
+    })
 
 })
 
 app.post("/signin",(req,res)=>{
+
+    const data= SigninSchema.safeParse(req.body);
+
+    if(!data.success){
+        res.json({
+            message:"Incorrect Input"
+        })
+        return
+    }
 
     const userId = 1;
     const token = jwt.sign({
@@ -24,6 +47,15 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/room",middleware,(req,res)=>{
+
+     const data= CreateRoomSchema.safeParse(req.body);
+
+    if(!data.success){
+        res.json({
+            message:"Incorrect Input"
+        })
+        return
+    }
     //db calls
     
 
